41a735544a51e35a7ea36754ff31dfb58e00ac52
LongNg2 <lnguyen@nortridge.com>
2023-04-06 16:59:42 -0700

    appointment component created

---
+++ src/components/Appointment.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Appointment = () => {
+    return (
+        <div>
+            <p> this is the appointment div </p>
+        </div>
+    )
+}
+
+
+export default Appointment;
